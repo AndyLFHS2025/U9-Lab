2bc96843f14957149abb8b66c6d3bbb946604d6e
AndyLFHS2025 <93545706+AndyLFHS2025@users.noreply.github.com>
2023-03-21 21:34:08 -0500

    Update Bacterial.java

--- Bacterial.java
+++ Bacterial.java
@@ -1,4 +1,5 @@
-package Unit9_Inheritance.Lab;
+package U9Lab;
+
 import java.util.Scanner;
 
 public class Bacterial extends Disease {
@@ -15,14 +16,27 @@ public class Bacterial extends Disease {
     }
 
     public String mutate(double mutFactor) {
+        double ogRes = resistance;
         String str;
-        this.resistance *= (mutFactor * Math.random());
+        this.resistance = (mutFactor * Math.random());
         if (resistance == 0.0) {
             str = "This strain of bacteria died out.";
         }
 
         else {
             str = "This bacteria's resistance is now " + (resistance * 100) + "%.";
+        }
+
+        if(ogRes < .5) {
+            if (resistance >= .5) {
+                reclassifyBacteria();
+            }
+        }
+
+        if(ogRes >= .5) {
+            if (resistance < .5) {
+                reclassifyBacteria();
+            }
         }
 
         return str;
@@ -35,25 +49,30 @@ public class Bacterial extends Disease {
         return str;
     }
 
-//    public void reclassifyBacteria(Bacterial newClass){
-//        if(resistance < 50.0) {
-//            System.out.println("The bacteria is now considered curable please input the recommended treatment and treatment length\nTreatment Name: ");
-//            Scanner input = new Scanner(System.in);
-//            String treatment = input.nextLine();
-//            System.out.println("Treatment Length: ");
-//            int len = input.nextInt();
-//            input.nextLine();
-//            newClass = new CurableBacteria(getSurvivability(), getNumCases(), getYearDiscovered(), getOrigin(), resistance, getName(), treatment, len);
-//
-//        }
-//        else if(resistance > 50){
-//            System.out.println("The bacteria is now considered incurable please input the normal length of the bacterial infection");
-//            Scanner input = new Scanner(System.in);
-//            int sickTime = input.nextInt();
-//            input.nextLine();
-//            IncurableBacteria newIncurable = new IncurableBacteria(getSurvivability(), getNumCases(), getYearDiscovered(), getOrigin(), resistance, getName(), sickTime);
-//        }
-//    }
+   public Bacterial reclassifyBacteria(){
+        Bacterial newClass = new Bacterial();
+
+        if(resistance < .5 ) {
+            System.out.println("The bacteria is curable, please input the recommended treatment and treatment length\nTreatment Name: ");
+            Scanner input = new Scanner(System.in);
+            String treatment = input.nextLine();
+            System.out.println("Treatment Length: ");
+            int len = input.nextInt();
+            input.nextLine();
+            newClass = new CurableBacteria(getSurvivability(), getNumCases(), getYearDiscovered(), getOrigin(),
+                    getNumSurvived(), getNumDeaths(), resistance, getName(), treatment, len);
+        }
+
+        else if(resistance >= .5){
+            System.out.println("The bacteria is incurable please input the normal length of the bacterial infection");
+            Scanner input = new Scanner(System.in);
+            int sickTime = input.nextInt();
+            input.nextLine();
+            newClass = new IncurableBacteria(getSurvivability(), getNumCases(), getYearDiscovered(), getOrigin(),
+                    getNumSurvived(), getNumDeaths(), resistance, getName(), sickTime);
+        }
+        return newClass;
+    }
 
     public double isResistant() {
         return resistance;
