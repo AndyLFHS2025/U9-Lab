f675c24d3d4c66ed23a641f6152f197951a1bd0b
AndyLFHS2025 <93545706+AndyLFHS2025@users.noreply.github.com>
2023-03-21 21:35:17 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,10 +1,13 @@
-package Unit9_Inheritance.Lab;
+package U9Lab;
 
 public class Driver {
     public static void main(String[] args) {
-        Disease vTest = new Viral(.5, 100, 100, "mars", 50, 25, true, "DNA", "your mom");
-        Disease bTest = new Bacterial(.5, 100, 100, "june", .5, "your dad", 10, 20);
-        Disease ncbTest = new IncurableBacteria(.5, 100, 2022, "Wuhan", 10, 20, 1.00, "Covid", 10);
-        Disease cbTest = new CurableBacteria()
+        Viral vTest = new Viral(.5, 100, 100, "mars", 50, 25, true, "DNA", "your mom");
+        Bacterial bTest = new Bacterial(.5, 100, 100, "june", .5, "your dad", 10, 20);
+        IncurableBacteria ncbTest = new IncurableBacteria(.5, 100, 2022, "Wuhan", 10, 20, 1.00, "Covid", 10);
+        CurableBacteria cbTest = new CurableBacteria(.5, 10, 2060, "Venus", 10, 20, .25, "your dad", "nuts", 100);
+
+        System.out.println(bTest.mutate(1));
+
     }
 }
